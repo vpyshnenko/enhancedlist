65a99d67128828134d7c61a442074505a61d85cc
Viktor Pyshnenko <viktor.pyshnenko@tiny.cloud>
2020-12-30 22:39:14 +0200

    initialData

--- src/main/ts/ui/Dialog.ts
+++ src/main/ts/ui/Dialog.ts
@@ -1,12 +1,62 @@
 import { Editor } from "tinymce";
 import { getParentList } from "../core/Selection";
 import { getListStyleTypeItems, isOrderedListType } from "./ListStyleTypes";
+// import { SugarElement, SugarDocument, Traverse, Insert,  } from '@ephox/sugar';
+import { SelectorFilter, SugarElement, SelectorFind, Css } from "@ephox/sugar";
+import { Optional, Fun, Arr } from "@ephox/katamari";
 
 const open = (editor: Editor) => {
   const dom = editor.dom;
+  const currentList = Optional.from(editor.selection.getStart(true))
+    .map(SugarElement.fromDom)
+    .bind((elm) => SelectorFind.closest(elm, "ol,ul"));
+  const listStyleType = currentList.map((elm) =>
+    Css.get(elm, "list-style-type")
+  );
+  const indent = currentList
+    .bind((elm) => SelectorFind.child(elm, "li[style*='padding-left']"))
+    .map((elm) => Css.get(elm, "padding-left"));
+  const initialData = Arr.foldl(
+    [{ listStyleType }, { indent }],
+    (acc, item) => {
+      const [prop, opt] = Object.entries(item)[0];
+      opt.fold(Fun.noop, (v) => (acc[prop] = v));
+      return acc;
+    },
+    {}
+  );
+  const targetItems = [{ value: "current", text: "Current list" }];
+  currentList
+    .map((elm) => [
+      SelectorFilter.ancestors(elm, "ol,ul"),
+      SelectorFilter.descendants(elm, "ol,ul"),
+    ])
+    .map(([ancestors, descendants]) => {
+      if (ancestors.length > 0) {
+        targetItems.push({ value: "parent", text: "Current and parent lists" });
+      }
+      if (descendants.length > 0) {
+        targetItems.push({
+          value: "children",
+          text: "Current and children lists",
+        });
+      }
+      if (ancestors.length > 0 && descendants.length > 0) {
+        targetItems.push({ value: "all", text: "All lists" });
+      }
+    });
 
-  const currentList = getParentList(editor);
-
+  const targetSelectBoxSettings =
+    targetItems.length > 1
+      ? [
+          {
+            type: "selectbox",
+            name: "target",
+            label: "Apply styles only to",
+            items: targetItems,
+          },
+        ]
+      : [];
   editor.windowManager.open({
     title: "List style settings",
     body: {
@@ -23,28 +73,30 @@ const open = (editor: Editor) => {
           name: "indent",
           label: "Indent size",
         },
-        {
-          type: "selectbox",
-          name: "target",
-          label: "Apply styles only to",
-          items: [
-            { value: "current", text: "Current list" },
-            { value: "parent", text: "Current and parent lists" },
-            { value: "children", text: "Current and children lists" },
-            { value: "all", text: "All lists" },
-          ],
-        },
+        // {
+        //   type: "selectbox",
+        //   name: "target",
+        //   label: "Apply styles only to",
+        //   items: [
+        //     { value: "current", text: "Current list" },
+        //     { value: "parent", text: "Current and parent lists" },
+        //     { value: "children", text: "Current and children lists" },
+        //     { value: "all", text: "All lists" },
+        //   ],
+        // },
+        ...targetSelectBoxSettings,
       ],
     },
-    initialData: {
-      listStyleType: currentList
-        ? dom.getStyle(currentList, "list-style-type", true)
-        : "disc",
-      indent:
-        currentList && currentList.firstElementChild
-          ? dom.getStyle(currentList.firstElementChild, "padding-left")
-          : "",
-    },
+    initialData,
+    // initialData: {
+    //   listStyleType: currentList
+    //     ? dom.getStyle(currentList, "list-style-type", true)
+    //     : "disc",
+    //   indent:
+    //     currentList && currentList.firstElementChild
+    //       ? dom.getStyle(currentList.firstElementChild, "padding-left")
+    //       : "",
+    // },
     buttons: [
       {
         type: "cancel",
